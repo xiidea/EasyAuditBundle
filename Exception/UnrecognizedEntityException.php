5aae05e280604684cced281a8bed87172d397df1
Roni Saha <roni.cse@gmail.com>
2014-11-19 08:13:28 +0600

    code style

--- Exception/UnrecognizedEntityException.php
+++ Exception/UnrecognizedEntityException.php
@@ -1,9 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Only For Me
- * Date: 28-10-2014
- * Time: 10:47 PM
+
+/*
+ * This file is part of the XiideaEasyAuditBundle package.
+ *
+ * (c) Xiidea <http://www.xiidea.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Xiidea\EasyAuditBundle\Exception;
@@ -12,4 +15,4 @@ namespace Xiidea\EasyAuditBundle\Exception;
 class UnrecognizedEntityException extends \Exception {
 
     protected $message = "Entity must extend Xiidea\\EasyAuditBundle\\Entity\\BaseAuditLog";
-} 
+}
