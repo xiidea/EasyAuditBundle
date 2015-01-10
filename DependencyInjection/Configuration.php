ad6c9b59561fb3c68a0e49be4ee25d4198d3a611
Roni Saha <roni.cse@gmail.com>
2015-01-10 22:17:12 +0600

    Use self to access local static members

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -161,7 +161,7 @@ class Configuration implements ConfigurationInterface
     {
         $isExclusiveItem = 0 === strpos($element, '!');
 
-        Configuration::throwExceptionOnInvalid(!$isExclusiveItem === $isExclusiveList);
+        self::throwExceptionOnInvalid(!$isExclusiveItem === $isExclusiveList);
 
         $elements[] = $isExclusiveItem ? substr($element, 1) : $element;
         $isExclusiveList = $isExclusiveItem;
