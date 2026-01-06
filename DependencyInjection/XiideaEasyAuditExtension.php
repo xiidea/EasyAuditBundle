98e85550a7c778f8199d630d8c016cd4b53aac4e
saifulislamferoz <saif012@gmail.com>
2026-01-06 10:26:04 +0600

    fix: showing deprecations

--- DependencyInjection/XiideaEasyAuditExtension.php
+++ DependencyInjection/XiideaEasyAuditExtension.php
@@ -15,7 +15,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
